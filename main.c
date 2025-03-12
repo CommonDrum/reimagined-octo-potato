3b9fae5de3df9f6659f1c807ed80651a8cbcbb07
commondrum <karol.hetman@protonmail.com>
2025-03-12 23:19:52 +0000

    clicking the rect

--- main.c
+++ main.c
@@ -13,28 +13,38 @@ int main(int argc, char* argv[]) {
     int windowWidth, windowHeight;
     SDL_GetWindowSize(context->window, &windowWidth, &windowHeight);
 
+
+    SDL_FRect bar = {(float)windowWidth/2, 0.0f, 20.0f, 20.0f};
+
     while (!done) {
         SDL_Event event;
 
         SDL_SetRenderDrawColorFloat(context->renderer, 0.0f, 0.0f, 0.0f, 1.0f);
         SDL_RenderClear(context->renderer);
-
-        const int barWidth = 20;  
-        for (int x = 0; x < windowWidth; x += barWidth * 2) {
-            SDL_FRect bar = {(float)x, 0.0f, (float)barWidth, (float)windowHeight};
-
-            SDL_SetRenderDrawColorFloat(context->renderer, 1.0f, 0.0f, 0.0f, 1.0f); // Red
-            
-            SDL_RenderFillRect(context->renderer, &bar);
-        }
-
-        SDL_RenderPresent(context->renderer);
-
+        SDL_SetRenderDrawColorFloat(context->renderer, 1.0f, 0.0f, 0.0f, 1.0f); // Red
+        
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 done = true;
             }
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+                int mouseX = event.button.x;
+                int mouseY = event.button.y;
+
+
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    bar.x = mouseX;
+                    bar.y = mouseY;
+
+                } else if (event.button.button == SDL_BUTTON_RIGHT) {
+                }
+            }
         }
+
+        SDL_RenderFillRect(context->renderer, &bar);
+
+        SDL_RenderPresent(context->renderer);
+
     }
 
     sdl_cleanup(context);
